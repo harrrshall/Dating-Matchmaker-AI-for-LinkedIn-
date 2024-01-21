596639f4259c1325eed6b149ddb50ef646d437b2
CyberNovas <92268499+harrrshall@users.noreply.github.com>
2024-01-22 00:00:15 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const fs = require('fs');const fs = require('fs');
 
 function timeout(miliseconds) {
   return new Promise((resolve) => {
